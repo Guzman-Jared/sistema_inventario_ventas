32fb3ffc7a1b1d3efc47eda770fe1f4ece3a2639
Bryan Jared Guzman Amaya <6051236@incb.edu.sv>
2026-05-11 21:20:28 -0600

    Documentadas consultas UPDATE y DELETE en el archivo SQL

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -1,2 +1,6 @@
 INSERT INTO productos (nombre, stock, precio) 
 VALUES ('Laptop Gaming', 10, 850.50);
+
+UPDATE productos SET precio = 720.00, stock = 15 WHERE id = 1;
+
+DELETE FROM productos WHERE id = 3;
